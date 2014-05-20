7ff9c4bec68fc97c3623a7d474d149622b17f392
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-05-20 01:49:06 -0500

    add

--- findSource/AlchemyTest/alchemytest.py
+++ findSource/AlchemyTest/alchemytest.py
@@ -13,8 +13,9 @@ def readArticle(myUrl):
 
     response = alchemyapi.title("url", myUrl)
     output['title'] = response['title'].encode('utf-8', 'ignore')
-	if output['title'] =='':
-		output['title'] = 'No title found'     
+    if output['title'] =='':
+    	output['title']='No title found'
+
 
     response = alchemyapi.author("url", myUrl)
 
