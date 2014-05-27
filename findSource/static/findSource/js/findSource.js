f3ced7d07fceb9fbebbf2747e6f3ba2a12d76451
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-05-27 18:54:52 -0500

    add

--- findSource/static/findSource/js/findSource.js
+++ findSource/static/findSource/js/findSource.js
@@ -1,8 +1,27 @@
 $("#search").click(function (){
     var input = $('#user-input').val();
     if (input){
+
+
+    var percent=0; //百分比进度，显示在滚动条后面
+    var element="||"; //滚动条单元竖线
+    var elements="||"; //滚动条当前竖线
+    count(); //开始调用循环
+
+    function count(){
+    percent=percent+10; //每次百分比加10
+    elements =elements + element; //滚动条当前竖线增加一个滚动条单元竖线
+    document.loading.bar.value=elements; //设置窗体loading表单中bar元素的当前值
+    document.loading.percentage.value=percent+"%"; //设置窗体loading表单中percentage元素的当前值
+    if (percent<99){ //percent小于99则继续循环
+    setTimeout("count()",500); //每500ms进行一次count()
+    }
+    else{
         current_url = window.location.pathname;
         window.location.href = current_url  + input + '/result';
+    }
+    }
+
     }
 });
 
