8aeb544dec47f72687e77469c805d5cc7311618f
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-05-28 06:15:31 -0500

    add

--- findSource/static/findSource/js/findSource.js
+++ findSource/static/findSource/js/findSource.js
@@ -2,7 +2,7 @@ $("#search").click(function (){
     var input = $('#user-input').val();
     if (input){
         current_url = window.location.pathname;
-        window.location.href = 'www.google.com/';        
+        location.href = 'https://www.google.com/';        
         // window.location.href = current_url  + input + '/result';
     }
 });
