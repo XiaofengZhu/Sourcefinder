2b2dd0d2f39a62913fd48931424bbc7ca9da22ac
XiaofengZhu <zhuxiaofengwww@gmail.com>
2014-05-28 20:23:34 -0500

    add

--- findSource/static/findSource/js/NameList.js
+++ findSource/static/findSource/js/NameList.js
@@ -18,9 +18,11 @@ $(document).ready(function(){
             + response['author'] + '<br>';
 
              $.each(response['people'] , function(key, value) {
-                           // alert(key + ":" + value); //output key and value
+                           append_content += response['people'][key] +'</p>';                           
+                           append_content += response['people'][key]['company'] +'</p>';
+                           append_content += response['people'][key]['job_title'] +'</p>';                           
                            append_content += response['people'][key]['quotation'] +'</p>';
-
+                           append_content += response['people'][key]['linkedInLink'] +'</p>';
                         });
 
             append_content += '</p>';
